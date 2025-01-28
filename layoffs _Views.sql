87ed87bcb4442fa86a709c52aa201fa7db0c233b
Aaliya Bai <76511919+AaliyaBai@users.noreply.github.com>
2025-01-28 21:46:06 +0000

    Add files via upload

---
+++ layoffs _Views.sql
@@ -0,0 +1,7 @@
+SELECT * FROM max_min_Year_layoffs;
+SELECT * FROM Top_Companies_laid_off ;
+SELECT * FROM  Top_Industry_layoffs ;
+SELECT * FROM  Top_Country_layoffs;
+SELECT * FROM  Yearly_layoffs ; 
+SELECT * FROM monthly_layoff_progress;
+SELECT * FROM Top_layoffs_each_year;
